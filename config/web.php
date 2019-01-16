27f0faecc0ca968e27797a16754cb8b1177b6813
cep11p <cep11p@gmail.com>
2019-01-16 12:51:09 -0300

    TG-14 Se instala la extension giiant y se configura https://tree.taiga.io/project/cep11p-gestor-de-beneficios/us/14?no-milestone=1

--- config/web.php
+++ config/web.php
@@ -43,14 +43,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
+            'enableStrictParsing' => false,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
 ];
@@ -67,6 +68,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => ['*'],
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
