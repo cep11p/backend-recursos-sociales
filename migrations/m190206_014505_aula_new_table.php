77da3f8699f401ef286a0398665f3302d4fb0a63
cep11p <cep11p@gmail.com>
2019-02-05 23:58:12 -0300

    TG-34 Se crea la tabla aula con FK a recurso https://tree.taiga.io/project/cep11p-gestor-de-beneficios/us/34?no-milestone=1

---
+++ migrations/m190206_014505_aula_new_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190206_014505_aula_new_table
+ */
+class m190206_014505_aula_new_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $table = "aula";
+        $this->createTable($table, [
+            'recurso_socialid'=> $this->integer()->notNull(),
+            'alumnoid'=> $this->integer()->notNull(),
+        ]);
+        
+        $this->addForeignKey('fk_aula_recurso_socialid', $table, 'recurso_socialid', 'recurso', 'id');
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190206_014505_aula_new_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190206_014505_aula_new_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
