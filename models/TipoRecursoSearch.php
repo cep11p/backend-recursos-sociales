c7fa3f0df69fca1b3dce55a4aa984392a6c6ee74
cep11p <cep11p@gmail.com>
2019-02-11 13:17:37 -0300

    TG-42 #Se realiza la funcion de tipo de recurso por programa
    https://tree.taiga.io/project/cep11p-gestor-de-beneficios/us/42?no-milestone=1

---
+++ models/TipoRecursoSearch.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\TipoRecurso;
+
+/**
+* TipoRecursoSearch represents the model behind the search form about `app\models\TipoRecurso`.
+*/
+class TipoRecursoSearch extends TipoRecurso
+{
+    /**
+    * @inheritdoc
+    */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['nombre'], 'safe'],
+        ];
+    }
+
+    /**
+    * @inheritdoc
+    */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+    * Creates data provider instance with search query applied
+    *
+    * @param array $params
+    *
+    * @return ActiveDataProvider
+    */
+    public function search($params)
+    {
+        $query = TipoRecurso::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ]);
+
+        $query->andFilterWhere(['like', 'nombre', $this->nombre]);
+
+        return $dataProvider;
+    }
+    
+    public function busquedadGeneral($params)
+    {
+        $query = TipoRecurso::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => $params['pagesize'],
+                'page' => (isset($params['page']) && is_numeric($params['page']))?$params['page']:0
+            ],
+        ]);
+
+        $this->load($params,'');
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ]);
+
+        $query->andFilterWhere(['like', 'nombre', $this->nombre]);
+        
+        if(isset($params['programaid'])){
+            $query->leftJoin("programa_has_tipo_recurso as pht", "id=pht.tipo_recursoid");
+            $query->andWhere(array('in', 'pht.programaid', $params['programaid']));
+        }
+
+        return $dataProvider;
+    }
+}
