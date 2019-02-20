9a4a3f5153408dd3aee35f48d768e0cb23ebbcda
cep11p <cep11p@gmail.com>
2019-02-20 13:13:23 -0300

    TG-59 #Se realiza test si los datos van vacios https://tree.taiga.io/project/cep11p-gestor-de-beneficios/us/59?no-milestone=1

---
+++ tests/unit/models/RecursoTest.php
@@ -0,0 +1,41 @@
+<?php 
+
+use app\models\Recurso;
+class RecursoTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+        
+    }
+
+    protected function _after()
+    {
+    }
+
+    public function testFallaDarBajaRecursoSiDatosVacios()
+    {
+        $this->tester->haveFixtures([
+            \app\tests\fixtures\RecursoFixture::className()
+        ]);
+        
+        $model = Recurso::findOne(['id'=>1]);  
+        
+        $model->scenario = $model::SCENARIO_BAJA;
+        
+        $errors = Array(
+                        'fecha_baja' => Array(
+                            0 => 'Fecha Baja cannot be blank.'
+                        ),
+                        'descripcion_baja' => Array(
+                            0 => 'Descripcion Baja cannot be blank.'
+                        )
+                    );
+        $model->validate();
+        $this->assertArraySubset($model->getErrors(),$errors);
+    }
+}
