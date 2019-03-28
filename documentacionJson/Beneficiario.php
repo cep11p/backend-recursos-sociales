80c88eb1c57a7c824b9903864b60a2d22cd7b11c
cep11p <cep11p@gmail.com>
2019-03-28 12:42:02 -0300

    Se actualiza la documentacion

--- documentacionJson/Beneficiario.php
+++ documentacionJson/Beneficiario.php
@@ -7,248 +7,135 @@
 * global_param : busca por nombre, apellido y nro_documento
 * @method GET
 * @return arrayJson es una lista de beneficiarios
-    {
-        "success": "true",
-        "pagesize": 20,
-        "pages": 1,
-        "total_filtrado": 10,
-        "resultado": [
-            {
-                "monto": 49036.97,
-                "personaid": 1,
-                "recurso_cantidad": "4",
-                "persona": {
+{
+    "pagesize": 20,
+    "pages": 2,
+    "total_filtrado": 27,
+    "monto_acreditado": 32247.37,
+    "monto_baja": 36457.06,
+    "monto_sin_acreditar": 1275179.09,
+    "recurso_acreditado_cantidad": 3,
+    "recurso_baja_cantidad": 2,
+    "resultado": [
+        {
+            "personaid": 1,
+            "monto": 64826.61,
+            "monto_acreditado": 3212.23,
+            "monto_baja": 0,
+            "monto_sin_acreditar": 61614.38,
+            "recurso_cantidad": 5,
+            "recurso_baja_cantidad": 0,
+            "recurso_acreditado_cantidad": 1,
+            "persona": {
+                "id": 1,
+                "nombre": "Victoria Margarita",
+                "apellido": "González",
+                "apodo": "",
+                "nro_documento": "23851266",
+                "fecha_nacimiento": "1982-12-30",
+                "estado_civilid": 1,
+                "telefono": "2920430000",
+                "celular": "2920412227",
+                "sexoid": 2,
+                "tipo_documentoid": 1,
+                "nucleoid": 1,
+                "situacion_laboralid": 1,
+                "generoid": 2,
+                "email": "email22@correo.com",
+                "cuil": "20238512669",
+                "red_social": "redsocial1",
+                "estudios": [],
+                "sexo": "Mujer",
+                "genero": "Femenino",
+                "estado_civil": "Soltero/a",
+                "lugar": {
                     "id": 1,
-                    "nombre": "Victoria Margarita",
-                    "apellido": "González",
-                    "apodo": "",
-                    "nro_documento": "23851266",
-                    "fecha_nacimiento": "0000-00-00",
-                    "estado_civilid": null,
-                    "telefono": "",
-                    "celular": "2920412227",
-                    "sexoid": 2,
-                    "tipo_documentoid": null,
-                    "nucleoid": 1,
-                    "situacion_laboralid": null,
-                    "generoid": null,
-                    "email": "",
-                    "cuil": "20238512669",
-                    "red_social": "",
-                    "estudios": [],
-                    "sexo": "Mujer",
-                    "genero": "",
-                    "estado_civil": "",
-                    "lugar": {
-                        "id": 1,
-                        "nombre": "",
-                        "calle": "calle1",
-                        "altura": "100",
-                        "localidadid": 1,
-                        "latitud": "-1234123",
-                        "longitud": "21314124",
-                        "barrio": "barrio1",
-                        "piso": "0º",
-                        "depto": "A",
-                        "escalera": "",
-                        "entre_calle_1": "",
-                        "entre_calle_2": "",
-                        "localidad": "Capital Federal"
-                    }
-                }
-            },
-            {
-                ...Otro beficiario...
-            },
-            {
-                ...Otro beficiario...
-            },
-            {
-                "monto": 109234.15,
-                "personaid": 2,
-                "recurso_cantidad": "4",
-                "persona": {
-                    "id": 2,
-                    "nombre": "Isabel Sofía",
-                    "apellido": "Rodríguez",
-                    "apodo": "",
-                    "nro_documento": "32054238",
-                    "fecha_nacimiento": "0000-00-00",
-                    "estado_civilid": null,
-                    "telefono": "",
-                    "celular": "2920412228",
-                    "sexoid": 2,
-                    "tipo_documentoid": null,
-                    "nucleoid": 2,
-                    "situacion_laboralid": null,
-                    "generoid": null,
-                    "email": "",
-                    "cuil": "20320542389",
-                    "red_social": "",
-                    "estudios": [
-                        {
-                            "id": 1,
-                            "titulo": "Titulo fixture 1",
-                            "completo": 1,
-                            "en_curso": 0,
-                            "nivel_educativoid": 1,
-                            "nivel_educativo": "Pre-Escolar",
-                            "anio": "2001"
-                        },
-                        {
-                            "id": 2,
-                            "titulo": "Titulo fixture 2",
-                            "completo": 1,
-                            "en_curso": 0,
-                            "nivel_educativoid": 2,
-                            "nivel_educativo": "Primario",
-                            "anio": "2001"
-                        }
-                    ],
-                    "sexo": "Mujer",
-                    "genero": "",
-                    "estado_civil": "",
-                    "lugar": {
-                        "id": 2,
-                        "nombre": "",
-                        "calle": "calle2",
-                        "altura": "",
-                        "localidadid": 1,
-                        "latitud": "-1234122",
-                        "longitud": "21314125",
-                        "barrio": "barrio2",
-                        "piso": "1º",
-                        "depto": "B",
-                        "escalera": "",
-                        "entre_calle_1": "",
-                        "entre_calle_2": "",
-                        "localidad": "Capital Federal"
-                    }
+                    "nombre": "",
+                    "calle": "calle1",
+                    "altura": "100",
+                    "localidadid": 2538,
+                    "latitud": "-1234123",
+                    "longitud": "21314124",
+                    "barrio": "barrio1",
+                    "piso": "0º",
+                    "depto": "A",
+                    "escalera": "Entrecalle1",
+                    "entre_calle_1": "Entrecalle1",
+                    "entre_calle_2": "Entrecalle-103",
+                    "localidad": "El Bolson"
                 }
             }
-            
-        ]
-    }
+        },
+        {...},
+        {...},
+        {...},
+        {
+            "personaid": 4,
+            "monto": 99192.94,
+            "monto_acreditado": 0,
+            "monto_baja": 0,
+            "monto_sin_acreditar": 99192.94,
+            "recurso_cantidad": 5,
+            "recurso_baja_cantidad": 0,
+            "recurso_acreditado_cantidad": 0,
+            "persona": {
+                "id": 4,
+                "nombre": "Natalia Valentina",
+                "apellido": "Fernández",
+                "apodo": "",
+                "nro_documento": "25047036",
+                "fecha_nacimiento": "1982-12-27",
+                "estado_civilid": 4,
+                "telefono": "2920430003",
+                "celular": "2920412230",
+                "sexoid": 2,
+                "tipo_documentoid": 1,
+                "nucleoid": 4,
+                "situacion_laboralid": 1,
+                "generoid": 2,
+                "email": "email22@correo.com",
+                "cuil": "20250470369",
+                "red_social": "redsocial4",
+                "estudios": [
+                    {
+                        "id": 4,
+                        "titulo": "Titulo fixture 4",
+                        "completo": 1,
+                        "en_curso": 0,
+                        "nivel_educativoid": 4,
+                        "nivel_educativo": "Terciario",
+                        "anio": "2001"
+                    }
+                ],
+                "sexo": "Mujer",
+                "genero": "Femenino",
+                "estado_civil": "Viudo/a",
+                "lugar": {
+                    "id": 4,
+                    "nombre": "",
+                    "calle": "calle4",
+                    "altura": "",
+                    "localidadid": 2541,
+                    "latitud": "-1234120",
+                    "longitud": "21314127",
+                    "barrio": "barrio4",
+                    "piso": "3º",
+                    "depto": "D",
+                    "escalera": "Entrecalle4",
+                    "entre_calle_1": "Entrecalle4",
+                    "entre_calle_2": "Entrecalle-100",
+                    "localidad": "Rio Villegas"
+                }
+            }
+        }
+    ]
+}
 */
 
 /****** Para visualizar*****
 * @url http://recurso-social.local/api/beneficiario/{$id} 
 * @method GET
 * @return arrayJson es una benficiario con todas sus prestaciones
-    {
-       "id": 1,
-       "nombre": "Victoria Margarita",
-       "apellido": "González",
-       "nro_documento": "23851266",
-       "fecha_nacimiento": "0000-00-00",
-       "estado_civilid": null,
-       "telefono": "",
-       "celular": "2920412227",
-       "sexoid": 2,
-       "tipo_documentoid": null,
-       "nucleoid": 1,
-       "situacion_laboralid": null,
-       "generoid": null,
-       "email": "",
-       "cuil": "20238512669",
-       "red_social": "",
-       "estudios": [],
-       "sexo": "Mujer",
-       "genero": "",
-       "estado_civil": "",
-       "lugar": {
-           "id": 1,
-           "nombre": "",
-           "calle": "calle1",
-           "altura": "100",
-           "localidadid": 1,
-           "latitud": "-1234123",
-           "longitud": "21314124",
-           "barrio": "barrio1",
-           "piso": "0º",
-           "depto": "A",
-           "escalera": "",
-           "entre_calle_1": "",
-           "entre_calle_2": "",
-           "localidad": "Capital Federal"
-       },
-       "recurso_lista": {
-           "emprender": [
-               {
-                   "id": 1,
-                   "fecha_inicial": "2016-01-30",
-                   "fecha_alta": "2014-10-07",
-                   "monto": 3212.23,
-                   "observacion": "Observacion Fixture 1",
-                   "proposito": "Un proposito hecho con fixtures 1",
-                   "programaid": 3,
-                   "tipo_recursoid": 1,
-                   "personaid": 1,
-                   "fecha_baja": "2019-01-10",
-                   "fecha_acreditacion": null,
-                   "descripcion_baja": "una baja de ejemplo",
-                   "programa": "Emprender",
-                   "tipo_recurso": "Alimentación"
-               },
-               {
-                   "id": 31,
-                   "fecha_inicial": "2016-01-11",
-                   "fecha_alta": "2016-05-08",
-                   "monto": 13245.5,
-                   "observacion": "Observacion Fixture 31",
-                   "proposito": "Un proposito hecho con fixtures 31",
-                   "programaid": 3,
-                   "tipo_recursoid": 1,
-                   "personaid": 1,
-                   "fecha_baja": null,
-                   "fecha_acreditacion": null,
-                   "descripcion_baja": null,
-                   "programa": "Emprender",
-                   "tipo_recurso": "Alimentación"
-               }
-           ],
-           "subsidio": [
-               {
-                   "id": 11,
-                   "fecha_inicial": "2016-01-20",
-                   "fecha_alta": "2016-05-28",
-                   "monto": 16789.6,
-                   "observacion": "Observacion Fixture 11",
-                   "proposito": "Un proposito hecho con fixtures 11",
-                   "programaid": 1,
-                   "tipo_recursoid": 2,
-                   "personaid": 1,
-                   "fecha_baja": null,
-                   "fecha_acreditacion": null,
-                   "descripcion_baja": null,
-                   "programa": "Subsidio",
-                   "tipo_recurso": "Empleo/Formación Laboral"
-               }
-           ],
-           "rio_negro_presente": [
-               {
-                   "id": 21,
-                   "fecha_inicial": "2016-01-11",
-                   "fecha_alta": "2016-05-18",
-                   "monto": 15789.64,
-                   "observacion": "Observacion Fixture 21",
-                   "proposito": "Un proposito hecho con fixtures 21",
-                   "programaid": 2,
-                   "tipo_recursoid": 3,
-                   "personaid": 1,
-                   "fecha_baja": null,
-                   "fecha_acreditacion": null,
-                   "descripcion_baja": null,
-                   "programa": "Río Negro Presente",
-                   "tipo_recurso": "Mejora Habitacional"
-               }
-           ]
-       }
-   }
+ {PENDIENTE}
 */
 
-/****** Para borrar una localidad *****
-* @url http://recurso-social.local/api//{$id} 
-* @method Delete
-* @return arrayJson
-*/
---
+++ documentacionJson/Programa.php
@@ -0,0 +1,69 @@
+<?php
+
+/**** obtener lista de Programas ***
+@url ejemplo http://recurso-social.local/api/programas
+@Method GET
+[
+    {
+        "id": 3,
+        "nombre": "Emprender",
+        "monto_acreditado": 19001.87,
+        "monto_baja": 0,
+        "monto_sin_acreditar": 408779.73,
+        "recurso_cantidad": 24,
+        "recurso_baja_cantidad": 0,
+        "recurso_acreditado_cantidad": 2,
+        "persona_cantidad": 20,
+        "monto": 427781.6
+    },
+    {
+        "id": 5,
+        "nombre": "Hábitat",
+        "monto_acreditado": 0,
+        "monto_baja": 0,
+        "monto_sin_acreditar": 0,
+        "recurso_cantidad": 0,
+        "recurso_baja_cantidad": 0,
+        "recurso_acreditado_cantidad": 0,
+        "persona_cantidad": 0,
+        "monto": 0
+    },
+    {
+        "id": 4,
+        "nombre": "Micro Emprendimiento",
+        "monto_acreditado": 0,
+        "monto_baja": 0,
+        "monto_sin_acreditar": 0,
+        "recurso_cantidad": 0,
+        "recurso_baja_cantidad": 0,
+        "recurso_acreditado_cantidad": 0,
+        "persona_cantidad": 0,
+        "monto": 0
+    },
+    {
+        "id": 2,
+        "nombre": "Río Negro Presente",
+        "monto_acreditado": 0,
+        "monto_baja": 16456.9,
+        "monto_sin_acreditar": 442282.73,
+        "recurso_cantidad": 23,
+        "recurso_baja_cantidad": 1,
+        "recurso_acreditado_cantidad": 0,
+        "persona_cantidad": 19,
+        "monto": 458739.63
+    },
+    {
+        "id": 1,
+        "nombre": "Subsidio",
+        "monto_acreditado": 13245.5,
+        "monto_baja": 20000.16,
+        "monto_sin_acreditar": 424116.63,
+        "recurso_cantidad": 24,
+        "recurso_baja_cantidad": 1,
+        "recurso_acreditado_cantidad": 1,
+        "persona_cantidad": 20,
+        "monto": 457362.29
+    }
+]
+
+**/
