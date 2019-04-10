cf29449c37cfb5cbba81ba8db85d443b4b723f20
cep11p <cep11p@gmail.com>
2019-04-10 13:23:08 -0300

    TG-29 # Se realiza un test para registrar un recurso de tipo subsidio https://tree.taiga.io/project/cep11p-gestor-de-beneficios/us/29?no-milestone=1

--- tests/api/recursoCest.php
+++ tests/api/recursoCest.php
@@ -19,6 +19,7 @@ class recursoCest
     {
         return [
             'recursos' => app\tests\fixtures\RecursoFixture::className(),
+//            'aulas' => app\tests\fixtures\AulaFixture::className(),
         ];
     }
     
@@ -146,4 +147,83 @@ class recursoCest
         
         
     }
+    
+    public function crearUnRecursoSubsidio(ApiTester $I)
+    {
+        $I->wantTo('Se crea un recurso de tipo Subsidio');
+        
+        $param = [            
+            "personaid"=> 21,
+            "programaid"=> 1,
+            "tipo_recursoid"=> 3,
+            "prosito"=> "Para comprar Alimentos",
+            "fecha_alta"=> "2018-02-02",
+            "monto"=> 220.300,
+            "observacion"=> "Actualmente sin trabajo"
+        ];
+        
+        $I->sendPOST('/api/recursos',$param);
+        $I->seeResponseContainsJson([
+            "message"=>"Se guarda una prestacion",
+            "success"=>true,
+            "data"=>["id"=>72]
+        ]);
+        $I->seeResponseCodeIs(200);
+        
+        $I->sendGET('/api/recursos/72');
+        $I->seeResponseContainsJson([
+            'id' => 72,
+            'fecha_inicial' => '2019-04-10',
+            'fecha_alta' => '2018-02-02',
+            'monto' => 220.30000000000001,
+            'observacion' => 'Actualmente sin trabajo',
+            'proposito' => null,
+            'programaid' => 1,
+            'tipo_recursoid' => 3,
+            'personaid' => 21,
+            'fecha_baja' => null,
+            'fecha_acreditacion' => null,
+            'descripcion_baja' => null,
+            'programa' => 'Subsidio',
+            'tipo_recurso' => 'Mejora Habitacional',
+            'baja' => false,
+            'acreditacion' => false,
+            'persona' => Array (),
+        ]);
+        
+    }
+//    public function crearUnRecursoEmprenderConAlumnos(ApiTester $I)
+//    {
+//        $I->wantTo('Se crea un recurso de tipo Emprender con Alumnos');
+//        
+//        $param = [            
+//            "personaid"=> 20,
+//            "programaid"=> 3,
+//            "tipo_recursoid"=> 3,
+//            "prosito"=> "Se desea armar un taller de apicultura",
+//            "fecha_alta"=> "2011-02-02",
+//            "monto"=> 220.300,
+//            "observacion"=> "Falta conseguir maquinas para la cosecha",
+//            "alumno_lista"=>[
+//                ["alumnoid"=>1],
+//                ["alumnoid"=>2],
+//                ["alumnoid"=>4],
+//                ["alumnoid"=>5],
+//                ["alumnoid"=>6],
+//                ["alumnoid"=>7],
+//                ["alumnoid"=>8],
+//                ["alumnoid"=>9]
+//            ]
+//        ];
+//        
+//        $I->sendPOST('/api/recursos',$param);
+//        $I->seeResponseContainsJson([
+//            "message"=>"Se guarda una prestacion",
+//            "success"=>true,
+//            "data"=>["id"=>123],
+//            'message' => '{"aula":[{"recursoid":["La co...."]}]}'
+//        ]);
+//        $I->seeResponseCodeIs(200);
+//        
+//    }
 }
