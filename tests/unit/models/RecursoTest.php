d21c9b23fbbc415a9a9f0fb94f1f9fe577c49851
cep11p <cep11p@gmail.com>
2019-04-15 11:37:58 -0300

    TG-79 #done #Se testea el formato de fecha que tienen los mensajes de error https://tree.taiga.io/project/cep11p-gestor-de-beneficios/us/79?no-milestone=1

--- tests/unit/models/RecursoTest.php
+++ tests/unit/models/RecursoTest.php
@@ -39,6 +39,47 @@ class RecursoTest extends \Codeception\Test\Unit
         $this->assertArraySubset($model->getErrors(),$errors);
     }
     
+    public function testFallaDarBajaRecursoSiFechaBajaEsMenorAFechaAlta()
+    {
+        $this->tester->haveFixtures([
+            \app\tests\fixtures\RecursoFixture::className()
+        ]);
+        
+        $model = Recurso::findOne(['id'=>1]);  
+        
+        $model->scenario = $model::SCENARIO_BAJA;
+        $model->fecha_baja = '2014-10-06';
+        $model->descripcion_baja = 'Esto es una descripcion de prueba';
+
+        $errors = Array(
+            'fecha_baja' => Array(
+                0 => 'La fecha de baja no puede ser menor a la fecha de alta 07/10/2014'
+            )
+        );
+        $model->validate();
+        $this->assertArraySubset($model->getErrors(),$errors);
+    }
+    
+    public function testFallaDarBajaRecursoSiFechaBajaEsMayorAHoy()
+    {
+        $this->tester->haveFixtures([
+            \app\tests\fixtures\RecursoFixture::className()
+        ]);
+        
+        $model = Recurso::findOne(['id'=>1]);
+        $model->scenario = $model::SCENARIO_BAJA;
+        $model->fecha_baja = date("Y-m-d",strtotime(date('Y-m-d')."+ 1 days"));
+        $model->descripcion_baja = 'Esto es una descripcion de ejemplo';
+        
+        $errors = Array(
+            'fecha_baja' => Array(
+                0 => 'La fecha de baja no puede ser mayor a la fecha de hoy '.date('d/m/Y')
+            )
+        );
+        $model->validate();
+        $this->assertArraySubset($model->getErrors(),$errors);
+    }
+    
     public function testFallaAcreditarRecursoSiDatosVacios()
     {
         $this->tester->haveFixtures([
@@ -53,6 +94,44 @@ class RecursoTest extends \Codeception\Test\Unit
                 0 => 'Fecha Acreditacion cannot be blank.'
             )
 
+        );
+        $model->validate();
+        $this->assertArraySubset($model->getErrors(),$errors);
+    }
+    
+    public function testFallaAcreditarSiFechaAcreditacionEsMenorAFechaAlta()
+    {
+        $this->tester->haveFixtures([
+            \app\tests\fixtures\RecursoFixture::className()
+        ]);
+        
+        $model = Recurso::findOne(['id'=>22]);  
+        $model->scenario = $model::SCENARIO_ACREDITACION;
+        $model->fecha_acreditacion = '2016-05-16';
+        
+        $errors = Array(
+            'fecha_acreditacion' => Array (
+                0 => 'La fecha de acreditacion no puede ser menor a la fecha de alta 17/05/2016'
+            )
+        );
+        $model->validate();
+        $this->assertArraySubset($model->getErrors(),$errors);
+    }
+    
+    public function testFallaAcreditarSiFechaAcreditacionEsMayorAHoy()
+    {
+        $this->tester->haveFixtures([
+            \app\tests\fixtures\RecursoFixture::className()
+        ]);
+        
+        $model = Recurso::findOne(['id'=>22]);
+        $model->scenario = $model::SCENARIO_ACREDITACION;
+        $model->fecha_acreditacion = date("Y-m-d",strtotime(date('Y-m-d')."+ 1 days"));
+        
+        $errors = Array(
+            'fecha_acreditacion' => Array(
+                0 => 'La fecha de acreditacion no puede ser mayor a la de hoy '.date('d/m/Y')
+            )
         );
         $model->validate();
         $this->assertArraySubset($model->getErrors(),$errors);
