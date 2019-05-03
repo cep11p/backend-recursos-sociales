6dded5bc2e8a7dd989584a901f00fcecdef608a1
cep11p <cep11p@gmail.com>
2019-05-03 13:50:08 -0300

    TG-106 #done #Se redefine la accion delele y se valida la integridad https://tree.taiga.io/project/cep11p-gestor-de-beneficios/us/106?no-milestone=1

--- modules/backend/controllers/TipoRecursoController.php
+++ modules/backend/controllers/TipoRecursoController.php
@@ -7,5 +7,29 @@ namespace app\modules\backend\controllers;
 */
 class TipoRecursoController extends \app\modules\backend\controllers\base\TipoRecursoController
 {
+     public function actions() {
+        $actions = parent::actions();
+        unset($actions['delete']);
+        return $actions;
+    }
+    
+    public function actionDelete($id) {
+        $model = \app\models\TipoRecurso::findOne(['id'=>$id]);
+        
+        
+        if($model==NULL){
+            \Yii::$app->session->setFlash('error','El tipo de recurso a borrar es incorrecta!!');
+        }
+        
+        if(count($model->programas)>0){
+            \Yii::$app->session->setFlash('error','No se pudo borrar el Tipo de recurso. La misma tiene vínculo con algunos Programas!!');
+        }else{
+            $model->delete(); 
+        }
+        
+        
+        
+        return $this->redirect(['/backend/tipo-recursos']);
+    }
 
 }
