db3ce5df4c055d7ca286a1e53ba605ecec64ce24
cep11p <cep11p@gmail.com>
2019-06-06 13:53:14 -0300

    TG-118 #Se define un template y se personaliza la fuentes y alineaciones del contenido https://tree.taiga.io/project/cep11p-gestor-de-beneficios/us/118?no-milestone=1

--- modules/api/controllers/RecursoController.php
+++ modules/api/controllers/RecursoController.php
@@ -8,9 +8,7 @@ use Yii;
 use yii\base\Exception;
 
 use app\models\Recurso;
-use app\models\Aula;
 
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\Writer\Pdf;
 
@@ -254,31 +252,69 @@ class RecursoController extends ActiveController{
             $params = \Yii::$app->request->queryParams;
             $resultado = $searchModel->busquedadGeneral($params);
             
-//            print_r($resultado);
-//            die();
+            #cargamos el temprate
+            $inputFileName = \Yii::$app->basePath.'/template-export/01template.xls';
+            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
             
-            $spreadsheet = new Spreadsheet();
             $sheet = $spreadsheet->getActiveSheet();
+                     
+            #Se detalla el total filtrado
+            $sheet->setCellValue('L6', 'Total filtrado: '.$resultado['total_filtrado']);
+            $sheet->getCell('L6')->getStyle()->getFont()->setBold(true);
+            $sheet->getCell('L6')->getStyle()->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+            
+            #Se detalla en negrita el monto acreditado y el monto sin acreditar
+            $sheet->setCellValue('I7', 'Monto acreditado: $'.$resultado['monto_acreditado']);            
+            $sheet->setCellValue('K7', 'Monto sin acreditar: $'.$resultado['monto_sin_acreditar']);  
+            
+            #se ponen en negrita el siguiente rango de celdas
+            $sheet->getStyle('I7:K7')->getFont()->setBold(true);
+            
+            #Se unen las celdas
+            $sheet->mergeCells('I7:J7');
+            $sheet->mergeCells('K7:L7');
+            
+            #Se alinea el texto
+            $sheet->getCell('I7')->getStyle()->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT);
+            $sheet->getCell('K7')->getStyle()->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_RIGHT);
+            
             
             /**** Cabecera de Persona*****/
-            $sheet->setCellValue('A1', 'Nro Documento');
-            $sheet->setCellValue('B1', 'Apellido y Nombre');
-            $sheet->setCellValue('C1', 'Localidad');
-            $sheet->setCellValue('D1', 'Dirección');
-            $sheet->setCellValue('E1', 'Contacto');
+            $cabeceraRow = 9;
+            $sheet->setCellValue('A'.$cabeceraRow, 'Nro Documento');
+            $sheet->setCellValue('B'.$cabeceraRow, 'Apellido y Nombre');
+            $sheet->setCellValue('C'.$cabeceraRow, 'Localidad');
+            $sheet->setCellValue('D'.$cabeceraRow, 'Dirección');
+            $sheet->setCellValue('E'.$cabeceraRow, 'Contacto');
             
             /**** Cabecera de Prestacion*****/
-            $sheet->setCellValue('F1', 'Programa');
-            $sheet->setCellValue('G1', 'Tipo Recurso');
-            $sheet->setCellValue('H1', 'Fecha Alta');
-            $sheet->setCellValue('I1', 'Monto');
-            $sheet->setCellValue('J1', 'Proposito');
-            $sheet->setCellValue('K1', 'Observacion');
-            $sheet->setCellValue('L1', 'Estado');
+            $sheet->setCellValue('F'.$cabeceraRow, 'Programa');
+            $sheet->setCellValue('G'.$cabeceraRow, 'Tipo Recurso');
+            $sheet->setCellValue('H'.$cabeceraRow, 'Fecha Alta');
+            $sheet->setCellValue('I'.$cabeceraRow, 'Monto');
+            $sheet->setCellValue('J'.$cabeceraRow, 'Propósito');
+            $sheet->setCellValue('K'.$cabeceraRow, 'Estado');
+            $sheet->setCellValue('L'.$cabeceraRow, 'Observación');
+            
+            $styleArray = [
+                'font' => [
+                    'bold' => true,
+                ],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+            ];
+            
+            #Se pone en negrita
+            $sheet->getStyle('A'.$cabeceraRow.':L'.$cabeceraRow)->applyFromArray($styleArray);
             
             /**** Contenido ****/
-            $fila = 2;
+            $fila = 10;
             foreach ($resultado['resultado'] as $value) {
+                #se alterna el color de fondo de las filas
+                if($fila%2==0){
+                    $spreadsheet->getActiveSheet()->getStyle('A'.$fila.':L'.$fila)->getFill()->setFillType(\PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID)->getStartColor()->setARGB('eeeeee');
+                }
                 
                 /***** Registros de Persona *****/
                 $sheet->setCellValue('A'.$fila, $value['persona']['nro_documento']);
@@ -287,20 +323,31 @@ class RecursoController extends ActiveController{
                 $sheet->setCellValue('D'.$fila, \app\components\Help::componerDireccion($value['persona']['lugar']));
                 $sheet->setCellValue('E'.$fila, \app\components\Help::componerContacto($value['persona']));
                 
+                #Se formatea el tipo de valor a contener
+                $sheet->getCell('E'.$fila)->setDataType(\PhpOffice\PhpSpreadsheet\Cell\DataType::TYPE_STRING);
+                
                 /***** Registros de Prestacion *****/
                 $sheet->setCellValue('F'.$fila, $value['programa']);
                 $sheet->setCellValue('G'.$fila, $value['tipo_recurso']);
                 $sheet->setCellValue('H'.$fila, $value['fecha_alta']);
                 $sheet->setCellValue('I'.$fila, '$'.$value['monto']);
                 $sheet->setCellValue('J'.$fila, $value['proposito']);
-                $sheet->setCellValue('L'.$fila, \app\components\Help::componerEstadoPrestacion($value));
+                $sheet->setCellValue('K'.$fila, \app\components\Help::componerEstadoPrestacion($value));
+                $sheet->setCellValue('L'.$fila, $value['observacion']);
                 
                 $fila++;
             }
-            $fila = $fila+2;
-            $sheet->setCellValue('J'.$fila, 'Monto acreditado: $'.$resultado['monto_acreditado']);
-            $sheet->setCellValue('K'.$fila, 'Monto sin acreditar: $'.$resultado['monto_sin_acreditar']);
             
+            #Se detalla en negrita el criterio de fecha_desde y fecha_hasta en el documento
+            $fila = $fila+2;
+            if(isset($params['fecha_desde']) && !empty($params['fecha_desde'])){
+                $cadena = 'Fecha desde '.$params['fecha_desde'];
+                $cadena .= (isset($params['fecha_hasta']) && !empty($params['fecha_hasta']))?' hasta '.$params['fecha_hasta']:'';
+                $sheet->setCellValue('A'.$fila, $cadena);  
+                $sheet->getCell('A'.$fila)->getStyle()->getFont()->setBold(true);
+            }
+            
+            #Se prepara la cabecera para exportar el archivo
             header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
             header('Content-Disposition: attachment;filename="prestaciones.xls"');
             header('Cache-Control: max-age=0');
