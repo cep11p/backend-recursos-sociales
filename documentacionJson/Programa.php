a9154fb5f45bdbdbf6e90309800def1ab75f5efe
cep11p <cep11p@gmail.com>
2019-07-29 13:20:19 -0300

    Se actualiza la documentacion Json

--- documentacionJson/Programa.php
+++ documentacionJson/Programa.php
@@ -5,6 +5,80 @@
 @Method GET
 [
     {
+        "id": 1,
+        "nombre": "Subsidio",
+        "activo": null,
+        "lista_tipo_recurso": [
+            {
+                "id": 1,
+                "nombre": "Alimentación"
+            },
+            {
+                "id": 3,
+                "nombre": "Mejora Habitacional"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "nombre": "Río Negro Presente",
+        "activo": null,
+        "lista_tipo_recurso": [
+            {
+                "id": 1,
+                "nombre": "Alimentación"
+            },
+            {
+                "id": 2,
+                "nombre": "Empleo/Formación Laboral"
+            },
+            {
+                "id": 3,
+                "nombre": "Mejora Habitacional"
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "nombre": "Emprender",
+        "activo": null,
+        "lista_tipo_recurso": [
+            {
+                "id": 2,
+                "nombre": "Empleo/Formación Laboral"
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "nombre": "Micro Emprendimiento",
+        "activo": null,
+        "lista_tipo_recurso": [
+            {
+                "id": 2,
+                "nombre": "Empleo/Formación Laboral"
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "nombre": "Hábitat",
+        "activo": null,
+        "lista_tipo_recurso": [
+            {
+                "id": 3,
+                "nombre": "Mejora Habitacional"
+            }
+        ]
+    }
+]
+ */
+
+/**** Estadisticas por programa ****
+@url ejemplo http://recurso-social.local/api/programas/detalle
+@Method GET
+[
+    {
         "id": 3,
         "nombre": "Emprender",
         "monto_acreditado": 19001.87,
