e63924fff27d1c2e6e66bf782aea29d7dc256383
cep11p <cep11p@gmail.com>
2019-08-07 12:14:40 -0300

    TG-152 Se sube la documentacionJson https://tree.taiga.io/project/cep11p-gestor-de-beneficios/us/152?no-milestone=1

---
+++ documentacionJson/Estadistica.php
@@ -0,0 +1,34 @@
+<?php
+
+/**** Se obtienen la cantidad de beneficiarios que hay en los programas clasificados por localidad ***
+@url ejemplo http://recurso-social.local/api/estadisticas/beneficiarios-por-programa-en-localidad/2640
+@Method GET
+[
+    {
+        "id": "3",
+        "localidadid": "2640",
+        "nombre": "Emprender",
+        "beneficiario_cantidad": "4"
+    },
+    {
+        "id": "5",
+        "localidadid": "2640",
+        "nombre": "Hábitat",
+        "beneficiario_cantidad": "2"
+    },
+    {
+        "id": "2",
+        "localidadid": "2640",
+        "nombre": "Río Negro Presente",
+        "beneficiario_cantidad": "2"
+    },
+    {
+        "id": "1",
+        "localidadid": "2640",
+        "nombre": "Subsidio",
+        "beneficiario_cantidad": "7"
+    }
+]
+ */
+
+
