1fea77ee760a1986a957579fff63d28c65bd2c3c
cep11p <cep11p@gmail.com>
2021-01-07 12:17:53 -0300

    se actualiza la documentacion Json

--- documentacionJson/Usuario.php
+++ documentacionJson/Usuario.php
@@ -104,17 +104,29 @@
 ]
 */
 
-/**** Crear Asiganaciones a usuarios*****
+/**** Crear Asiganaciones a Usuarios*****
 * @url http://prestaciones-sociales.local/api/usuarios/crear-asignacion 
 * @method POST
 * @param arrayJson
 {
 	"usuarioid": 15,
-	"rol": "usuario",
 	"programaid": "2",
 	"lista_permiso":[
 		{"name":"prestacion_ver"},
 		{"name":"prestacion_crear"}
     ]
+}
+
+/**** Borrar Asiganaciones a Usuarios*****
+* @url http://prestaciones-sociales.local/api/usuarios/borrar-asignacion 
+* @method POST
+* @param arrayJson
+{
+    "programaid": "1",
+    "lista_permiso": [
+        "prestacion_crear",
+        "prestacion_ver"
+    ],
+    "usuarioid": 2
 }
 **/
