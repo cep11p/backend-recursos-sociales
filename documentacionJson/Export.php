ffa10ba9148c1c750477333a55d03293e3964e00
cep11p <cep11p@gmail.com>
2021-02-09 10:15:12 -0300

    Se actualiza la documentacionJson

---
+++ documentacionJson/Export.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ **** Realiza una exportacion a excel****
+ * Se exporta una lista de prestaciones
+ * Se permiten todos los parametros para realzar un exportacion por criterio
+ * @url http://recurso-social.local/api/export/exportar-prestaciones-xls
+ * @method GET
+ * @Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+ */
--- documentacionJson/Recurso.php
+++ documentacionJson/Recurso.php
@@ -411,19 +411,11 @@
  */
 
 /**
- **** Realiza una baja de una recurso(prestacion) ****
+ **** Realiza una acreditacion de un recurso(prestacion) ****
  * @url http://recurso-social.local/api/recursos/acreditar/1
  * @method PUT
  * @param
     {
         "fecha_acreditacion": "2014-10-06"
     }
- */
-
-/**
- **** Realiza una exportacion a excel****
- * Se exporta una lista de prestaciones
- * Se permiten todos los parametros para realzar un exportacion por criterio
- * @url http://recurso-social.local/api/recurso/exportar-prestaciones-xls
- * @method GET
  */
